1faf593e2444a8806a7a2c36b420850997b3bb51
MarcusS919 <45082048+MarcusS919@users.noreply.github.com>
2018-12-14 16:15:47 -0800

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public event System.Action OnReachedEndOfLevel;
+
+    public float movespeed = 7;
+    public float smoothMoveTime = .1f;
+    public float turnSpeed = 8;
+
+    float angle;
+    float smoothInputMagnitude;
+    float smoothMoveVelocity;
+    Vector3 velocity;
+
+    Rigidbody rigidbody;
+    bool disabled;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+        Vector3 inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        float inputMagnitude = inputDirection.magnitude;
+        smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+
+        float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+        angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * turnSpeed * inputMagnitude);
+        transform.eulerAngles = Vector3.up * angle;
+
+        velocity = transform.forward * movespeed * smoothInputMagnitude;
+
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.MoveRotation(Quaternion.Euler(Vector3.up * angle));
+        rigidbody.MovePosition(rigidbody.position + velocity * Time.deltaTime);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            //to kill the player
+            //   Destroy(gameObject);
+            transform.position = new Vector3(-14, 0, -16);
+        }
+        if (collision.gameObject.tag == "Finish")
+        {
+            Disable();
+            if (OnReachedEndOfLevel != null)
+            {
+                OnReachedEndOfLevel();
+            }
+        }
+    }
+    void Disable()
+    {
+        disabled = true;
+    }
+}
---
+++ Victory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Victory : MonoBehaviour {
+
+    public GameObject gameWinUI;
+    bool gameIsOver;
+
+
+    void Start()
+    { 
+        FindObjectOfType<Player>().OnReachedEndOfLevel += ShowGameWinUI;
+    }
+
+    void Update()
+    {
+        if (gameIsOver)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+
+    void ShowGameWinUI()
+    {
+        OnGameOver(gameWinUI);
+    }
+
+    void OnGameOver(GameObject gameOverUI)
+    {
+        gameOverUI.SetActive(true);
+        gameIsOver = true;
+        FindObjectOfType<Player>().OnReachedEndOfLevel -= ShowGameWinUI;
+    }
+}
